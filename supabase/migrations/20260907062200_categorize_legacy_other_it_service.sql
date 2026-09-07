7e711aab8a2e7a7540776818bcb0ec7be110cce1
senid-221 <iraguhavincent11@gmail.com>
2026-09-06 22:38:05 -0700

    fix: place legacy other IT service in a real category

---
+++ supabase/migrations/20260907062200_categorize_legacy_other_it_service.sql
@@ -0,0 +1,3 @@
+begin;
+update public.services set category_slug='computers-laptops', category_name='Computers & Laptops', category_name_rw='Mudasobwa na Laptop' where slug='other-it-services';
+commit;
